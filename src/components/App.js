ddc65d3a8750f6488c36f012ee3a06ca72035213
Xiao <hexiao125@gmail.com>
2018-12-08 17:38:01 +0100

    Added video player

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
-import VideoDetial from './VideoDetail';
+import VideoDetail from './VideoDetail';
 
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
@@ -25,7 +25,7 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit} />
-                <VideoDetial video={this.state.selectedVideo} />
+                <VideoDetail video={this.state.selectedVideo} />
                 <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos} />
             </div>
         );
